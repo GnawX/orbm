153d9d479993123f51aff8a0a75dd4729888f05e
Xiaoming Wang <40604687+Xiaoming-Wang1@users.noreply.github.com>
2020-11-17 06:38:39 -0500

    Update greenfunction2.f90

--- src/greenfunction2.f90
+++ src/greenfunction2.f90
@@ -5,7 +5,7 @@
 ! or http://www.gnu.org/copyleft/gpl.txt .
 !
 !-----------------------------------------------------------------------
-SUBROUTINE greenfunction(ik, psi, g_psi)
+SUBROUTINE greenfunction2(ik, psi, g_psi)
   !-----------------------------------------------------------------------
   !
   ! ... Apply the Green function operator
@@ -63,87 +63,23 @@ SUBROUTINE greenfunction(ik, psi, g_psi)
   call allocate_bec_type(nkb, nbnd, becp)
 
   !====================================================================
-  ! apply -Q_{k+q} to the r.h.s.
+  ! apply -Q to the r.h.s.
   !====================================================================
   ! project on <evq|: ps(i,j) = <evq(i)|psi(j)>
   ps = (0.d0,0.d0)
-
-  if (lgauss) then
-     ! metallic case
-     if (noncolin) then
-         CALL zgemm('C', 'N', nbnd, nbnd_occ (ik), npwx*npol, (1.d0,0.d0), evc(1,1), &
-                     npwx*npol, psi(1,1), npwx*npol, (0.d0,0.d0), ps(1,1), nbnd)
-     else
-         CALL zgemm('C', 'N', nbnd, nbnd_occ (ik), npw, (1.d0,0.d0), evc(1,1), &
-                     npwx, psi(1,1), npwx, (0.d0,0.d0), ps(1,1), nbnd)
-     endif
   
-     do ibnd = 1, nbnd_occ(ik)
-        wg1 = wgauss ((ef-et(ibnd,ik)) / degauss, ngauss)
-        w0g = w0gauss((ef-et(ibnd,ik)) / degauss, ngauss) / degauss
-        do jbnd = 1, nbnd
-           wgp = wgauss ( (ef - et(jbnd,ik)) / degauss, ngauss)
-           deltae = et(jbnd,ik) - et(ibnd,ik)
-           theta = wgauss (deltae / degauss, 0)
-           wwg = wg1 * (1.d0 - theta) + wgp * theta
-           if (jbnd <= nbnd_occ(ik)) then
-              if (abs (deltae) > 1d-5) then
-                 wwg = wwg + alpha_pv * theta * (wgp - wg1) / deltae
-              else
-                 ! if the two energies are too close takes the limit of the 0/0 ratio
-                 wwg = wwg - alpha_pv * theta * w0g
-              endif
-           endif
-           ps(jbnd,ibnd) = wwg * ps(jbnd,ibnd)
-        enddo
-        call dscal(2*npwx*npol, wg1, psi(1,ibnd), 1)
-     enddo
-
-  else
-     ! insulators
-     if (noncolin) then
-     
-         CALL zgemm('C', 'N', nbnd_occ (ik), nbnd_occ (ik), npwx*npol, (1.d0,0.d0), evc(1,1), &
-                    npwx*npol, psi(1,1), npwx*npol, (0.d0,0.d0), ps(1,1), nbnd)
-     else
-         CALL zgemm('C', 'N', nbnd_occ (ik), nbnd_occ (ik), npw, (1.d0,0.d0), evc(1,1), &
-                    npwx, psi(1,1), npwx, (0.d0,0.d0), ps(1,1), nbnd)
-     endif
- 
-  endif
-
+  do ibnd = 1, nbnd
+     ps(ibnd) = zdotc(npwx*npol, evc(1,ibnd), 1, psi(1,ibnd), 1)
+  enddo
+  
 #ifdef __MPI
   call mp_sum(ps, intra_pool_comm)
 #endif
 
-  if (lgauss) then
-     ! metallic case
-     if (noncolin) then
-          CALL zgemm( 'N', 'N', npwx*npol, nbnd_occ(ik), nbnd, (1.d0,0.d0), &
-          evc(1,1), npwx*npol, ps(1,1), nbnd, (-1.d0,0.d0), psi(1,1), npwx*npol )
-     else
-          CALL zgemm( 'N', 'N', npw, nbnd_occ(ik), nbnd, (1.d0,0.d0), &
-          evc(1,1), npwx, ps(1,1), nbnd, (-1.d0,0.d0), psi(1,1), npwx )
-     endif
-
-  else
-     ! insulators
-     if (noncolin) then
-         CALL zgemm( 'N', 'N', npwx*npol, nbnd_occ(ik), nbnd_occ(ik), (1.d0,0.d0), &
-         evc(1,1), npwx*npol, ps(1,1), nbnd, (-1.d0,0.d0), psi(1,1), npwx*npol )
-     else
-         CALL zgemm( 'N', 'N', npw, nbnd_occ(ik), nbnd_occ(ik), (1.d0,0.d0), &
-         evc(1,1), npwx, ps(1,1), nbnd, (-1.d0,0.d0), psi(1,1), npwx )
-     endif
-
-  endif
-
-  !! this is the old code for norm-conserving:
-  !! |psi> = -(1 - |evq><evq|) |psi>
-  !!CALL zgemm('N', 'N', npw, nbnd_occ(ik), nbnd_occ(ik), &
-  !!           (1.d0,0.d0), evq(1,1), npwx, ps(1,1), nbnd, (-1.d0,0.d0), &
-  !!           psi(1,1), npwx)
-
+  do ibnd = 1, nbnd
+     psi(:,ibnd) = evc(:,ibnd)*ps(ibnd) - psi(:,ibnd)
+  enddo
+  
 
   !====================================================================
   ! solve the linear system (apply G_{k+q})
@@ -212,4 +148,137 @@ SUBROUTINE greenfunction(ik, psi, g_psi)
   deallocate (work, h_diag, eprec, ps)
   call deallocate_bec_type (becp)
 
-END SUBROUTINE greenfunction
+END SUBROUTINE greenfunction2
+
+
+subroutine ch_psi_all2 (n, h, ah, e, ik, m)
+  !-----------------------------------------------------------------------
+  !
+  ! This routine applies the operator ( H - \epsilon S + alpha_pv P_v)
+  ! to a vector h. The result is given in Ah.
+  !
+  USE kinds,        ONLY : dp
+  USE wvfct,        ONLY : npwx, nbnd
+  USE uspp,         ONLY : vkb
+!  USE becmod,       ONLY : becp, calbec
+  USE orbm_module, ONLY : nbnd_occ, alpha_pv
+  USE mp_pools,     ONLY : intra_pool_comm
+  USE mp,           ONLY : mp_sum
+  USE noncollin_module,      ONLY : noncolin, npol
+  USE wavefunctions,         ONLY : evc
+  
+  implicit none
+
+  integer :: n, m, ik
+  ! input: the dimension of h
+  ! input: the number of bands
+  ! input: the k point
+  real(DP) :: e (m)
+  ! input: the eigenvalue
+
+  complex(DP) :: h (npwx*npol, m), ah (npwx*npol, m)
+  ! input: the vector
+  ! output: the operator applied to the vector
+  !
+  !   local variables
+  !
+  integer :: ibnd, ikq, ig
+  ! counter on bands
+  ! the point k+q
+  ! counter on G vetors
+
+  complex(DP), allocatable :: ps (:), hpsi (:,:), spsi (:,:)
+  ! scalar products
+  ! the product of the Hamiltonian and h
+  ! the product of the S matrix and h
+
+  call start_clock ('ch_psi')
+  allocate (ps  ( nbnd , m))    
+  allocate (hpsi( npwx*npol , m))    
+  allocate (spsi( npwx*npol , m))    
+  hpsi (:,:) = (0.d0, 0.d0)
+  spsi (:,:) = (0.d0, 0.d0)
+  !
+  !   compute the product of the hamiltonian with the h vector
+  !
+
+  call h_psi ( npwx, n, m, h, hpsi )
+  CALL s_psi ( npwx, n, m, h, spsi )
+  
+  call start_clock ('last')
+  !
+  !   then we compute the operator H-epsilon S
+  !
+  ah = (0.d0,0.d0)
+
+  do ibnd = 1, m
+     do ig = 1, n
+        ah (ig, ibnd) = hpsi (ig, ibnd) - e (ibnd) * spsi (ig, ibnd)
+     enddo
+  enddo
+  IF (noncolin) THEN
+     DO ibnd = 1, m
+        DO ig = 1, n
+           ah(ig+npwx, ibnd) = hpsi(ig+npwx, ibnd) - e(ibnd) * spsi(ig+npwx, ibnd)
+        ENDDO
+     ENDDO
+  ENDIF
+  !
+  !   Here we compute the projector in the valence band
+  !
+  ikq = ik
+  ps (:) = (0.d0, 0.d0)
+  do ibnd = 1, nbnd
+     
+  if (noncolin) then
+     CALL zgemm ('C', 'N', nbnd_occ (ikq) , m, npwx*npol, (1.d0, 0.d0) , evc, &
+            npwx*npol, spsi, npwx*npol, (0.d0, 0.d0) , ps, nbnd)
+  else
+     call zgemm ('C', 'N', nbnd_occ (ikq) , m, n, (1.d0, 0.d0) , evc, &
+            npwx, spsi, npwx, (0.d0, 0.d0) , ps, nbnd)
+  endif
+  ps (:,:) = ps(:,:) * alpha_pv
+
+#ifdef __MPI
+  call mp_sum ( ps, intra_pool_comm )
+#endif
+
+  hpsi (:,:) = (0.d0, 0.d0)
+  if (noncolin) then
+      CALL zgemm ('N', 'N', npwx*npol, m, nbnd_occ (ikq) , (1.d0, 0.d0) , evc, &
+            npwx*npol, ps, nbnd, (1.d0, 0.d0) , hpsi, npwx*npol)
+  else
+      call zgemm ('N', 'N', n, m, nbnd_occ (ikq) , (1.d0, 0.d0) , evc, &
+            npwx, ps, nbnd, (1.d0, 0.d0) , hpsi, npwx)
+  endif
+  spsi(:,:) = hpsi(:,:)
+
+  !
+  !    And apply S again
+  !
+
+  !call calbec(n, vkb, hpsi, becp, m)
+  !call s_psi (npwx, n, m, hpsi, spsi)
+
+
+  do ibnd = 1, m
+     do ig = 1, n
+        ah (ig, ibnd) = ah (ig, ibnd) + spsi (ig, ibnd)
+     enddo
+  enddo
+  IF (noncolin) THEN
+       DO ibnd = 1, m
+          DO ig = 1, n
+             ah (ig+npwx, ibnd) = ah (ig+npwx, ibnd) + spsi (ig+npwx, ibnd)
+          ENDDO
+       ENDDO
+   END IF
+
+  deallocate (spsi)
+  deallocate (hpsi)
+  deallocate (ps)
+  call stop_clock ('last')
+  call stop_clock ('ch_psi')
+  return
+end subroutine ch_psi_all2
+
