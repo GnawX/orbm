7bc5c97f9f064c1d20f6878843c3c5d80efc8ac8
Xiaoming Wang <40604687+Xiaoming-Wang1@users.noreply.github.com>
2020-12-06 00:00:53 -0500

    Update epsilon.f90

--- src/epsilon.f90
+++ src/epsilon.f90
@@ -6,13 +6,13 @@
 ! or http://www.gnu.org/copyleft/gpl.txt .
 !
 !-----------------------------------------------------------------------
-SUBROUTINE calc_elec_dipole
+SUBROUTINE epsilon
   !-----------------------------------------------------------------------
   !
-  ! This routine calculates the electric transition dipole matrix
-  !                         - <n|r|m>
+  ! This routine calculates the dielectric function
   !
   USE kinds,                  ONLY : dp
+  USE cell_base,              ONLY : omega
   USE io_global,              ONLY : stdout, ionode, ionode_id
   USE io_files,               ONLY : nwordwfc, iunwfc
   USE wavefunctions,          ONLY : evc
@@ -20,7 +20,8 @@ SUBROUTINE calc_elec_dipole
   USE klist,                  ONLY : nks, nkstot, ngk
   USE wvfct,                  ONLY : nbnd, npwx, et
   USE lsda_mod,               ONLY : nspin
-  USE orbm_module,            ONLY : ry2ha, ci, vel_evc, eta
+  USE orbm_module,            ONLY : ry2ha, ci, vel_evc, eta, emin, emax, &
+                                     ne, pi
   USE buffers,                ONLY : get_buffer
   USE mp_pools,               ONLY : my_pool_id, me_pool, root_pool,  &
                                      inter_pool_comm, intra_pool_comm, npool
@@ -31,13 +32,14 @@ SUBROUTINE calc_elec_dipole
   IMPLICIT NONE
 
   ! the following three quantities are for norm-conserving PPs
-  complex(dp), allocatable, dimension(:,:,:,:) :: rmat_          ! 
-  complex(dp), allocatable, dimension(:,:,:,:) :: rmat          !
-  complex(dp), allocatable, dimension(:,:,:) :: ps            ! <n|v|m> 
-
+  complex(dp), allocatable, dimension(:,:,:) :: vmat            ! <n|v|m> 
+  
+  real(dp) :: eps_imag(ne, 6)
+  
   integer :: ik, ios, iunout
-  integer :: i, ibnd, jbnd
-  real(dp), external :: get_clock
+  integer :: i, j, ibnd, jbnd, n, ie
+  real(dp) :: det, egrid(ne), pref
+  real(dp), external :: get_clock, delta
   integer, external :: find_free_unit
   integer :: npw
 
@@ -46,29 +48,22 @@ SUBROUTINE calc_elec_dipole
   !-----------------------------------------------------------------------
   ! allocate memory
   !-----------------------------------------------------------------------
-  allocate ( ps(nbnd, nbnd, 3))
-  allocate ( rmat(nbnd,nbnd,nkstot,3),  rmat_(nbnd,nbnd, nks, 3) )
+  allocate ( vmat(nbnd, nbnd, 3), vel_vec(nbnd, nbnd, 3))
 
   ! print memory estimate
   call orbm_memory_report
 
-  write(stdout, '(5X,''Computing the electric dipole matrix (e bohr):'',$)')
+  write(stdout, '(5X,''Computing the dielectric function (e bohr):'',$)')
   write(stdout, *)
-
-
+ 
+  do i = 1, ne
+     egrid(i) = emin + (emax-emin)/(ne-1)*(i-1)
+  enddo
+  eps_imag = 0.d0 
   !====================================================================
   ! loop over k-points on the pool
   !====================================================================
   do ik = 1, nks
-
-#ifdef __MPI
-    if (me_pool == root_pool) &
-    write(*, '(5X,''k-point #'',I5,'' of '',I5,6X,''pool #'',I3,4X,''cpu time:'',F10.1)') &
-      ik, nks, my_pool_id+1, get_clock('orbm')
-#else
-    write(stdout, '(5X,''k-point #'',I5,'' of '',I5,4X,''cpu time:'',F10.1)') &
-      ik, nks, get_clock('orbm')
-#endif
 
     npw = ngk(ik)
 
@@ -78,66 +73,73 @@ SUBROUTINE calc_elec_dipole
     
     ! calculate du/dk    
     vel_evc(:,:,:) = (0.d0,0.d0)
-    ps(:,:,:)= (0.d0,0.d0)
+    vmat(:,:,:)= (0.d0,0.d0)
     do i = 1,3
        call apply_vel(evc, vel_evc(1,1,i), ik, i)
-       !aux(:,:) = vel_evc(:,:,i)
-       !call greenfunction(ik, aux, evc1(1,1,i))
        
        ! calculate the velocity matrix ps(nbnd,nbnd)
        if (noncolin) then
      
           CALL zgemm('C', 'N', nbnd, nbnd, npwx*npol, (1.d0,0.d0), evc(1,1), &
-                    npwx*npol, vel_evc(1,1,i), npwx*npol, (0.d0,0.d0), ps(1,1,i), nbnd)
+                    npwx*npol, vel_evc(1,1,i), npwx*npol, (0.d0,0.d0), vmat(1,1,i), nbnd)
        else
        
           CALL zgemm('C', 'N', nbnd, nbnd, npw, (1.d0,0.d0), evc(1,1), &
-                    npwx, vel_evc(1,1,i), npwx, (0.d0,0.d0), ps(1,1,i), nbnd)
+                    npwx, vel_evc(1,1,i), npwx, (0.d0,0.d0), vmat(1,1,i), nbnd)
        endif
        
     enddo
+   
     
 #ifdef __MPI
     call mp_sum(ps, intra_pool_comm)
 #endif 
     
-    ! electric dipole matrix 
-    ! <n|r|m> = i\hbar <n|v|m> / (e_m - e_n)
-    
-    do ibnd = 1, nbnd
-       do jbnd = 1, nbnd
-          if ( abs(et(ibnd,ik)-et(jbnd,ik)) < eta )  then
-             rmat_(jbnd, ibnd, ik, :) = (0.d0,0.d0)
-          else
-             rmat_(jbnd, ibnd, ik, :) = ps(jbnd, ibnd, :)*ci/ & 
-                                (et(ibnd,ik) - et(jbnd,ik))/ry2ha
-          endif
+    do ibnd = 1, nbnd_occ(ik)
+       do jbnd = nbnd_occ(ik)+1, nbnd
+          det = (et(jbnd,ik) - et(ibnd,ik))*ry2ha   
+          if (det < emax + 8*sigma) then
+          ! xx xy xz yy yz zz
+             n = 0
+             do i = 1,3
+                do j = i,3
+                   n = n + 1
+                   do ie = 1, ne
+                      if (abs(det-egrid(ie)) < 8*sigma) then
+                         pref = 4*pi**2/omega/egrid(ie)**2
+                         eps_imag(ie,n) = eps_imag(ie,n) + pref*CONJG(vmat(jbnd,ibnd,j))* &
+                                          vmat(jbnd,ibnd,i)*delta(det-egrid(ie))
+                      endif
+                   enddo
+                enddo
+             enddo
+          endif        
        enddo
     enddo
+    
+
   enddo ! ik
   
-  rmat_ = rmat_*(-1.0_DP)
-
-  if ( npool == 1 ) then
-     rmat = rmat_
-  else
-     call poolcollect_z( nbnd, nks, rmat_, nkstot, rmat)
-  endif
+#ifdef __MPI
+    call mp_sum(eps_imag, inter_pool_comm)
+#endif 
   
   ios = 0
   if ( ionode ) then
      iunout = find_free_unit( )
-     open (unit = iunout, file = 'edipole', status = 'unknown', form = &
-          'unformatted', iostat = ios)
+     open (unit = iunout, file = 'epsilon_imag.dat', status = 'unknown', iostat = ios)
      rewind (iunout)
   endif
 
   call mp_bcast (ios, ionode_id, world_comm)
-  if ( ios/=0 ) call errore ('calc_elec_dipole', 'Opening file edipole', abs (ios) )
+  if ( ios/=0 ) call errore ('epsilon', 'Opening file epsilon_imag.dat', abs (ios) )
 
   if (ionode) then
-     write(iunout) nbnd, nkstot, nspin
-     write(iunout) rmat
+     write(iunout, '7A10') 'ENERGY','XX','YY','ZZ','XY','XZ','YZ' 
+     do ie = 1, ne
+        write(iunout, '(7F10.4)') egrid(ie)*ry2ev/ry2ha, eps_imag(ie,1),eps_imag(ie,4), &
+                       eps_imag(ie,6), eps_imag(ie,2),eps_imag(ie,3),eps_imag(ie,5)
+     enddo
      close(iunout)
   endif
   
@@ -145,16 +147,15 @@ SUBROUTINE calc_elec_dipole
   ! print out results
   !====================================================================
   write(stdout,*)
-  write(stdout,'(5X,''End of electric dipole calculation'')')
-  write(stdout,*)
-  write(stdout,'(5X,''Matrix elements dumped in edipole'')')
+  write(stdout,'(5X,''End of dielectric function calculation'')')
   write(stdout,*)
 
+
   ! free memory as soon as possible
-  deallocate( vel_evc, ps, rmat, rmat_ )
+  deallocate( vel_evc, vmat)
 
   
   !call restart_cleanup ( )
-  call stop_clock('calc_elec_dipole')
+  call stop_clock('epsilon')
 
-END SUBROUTINE calc_elec_dipole
+END SUBROUTINE epsilon
