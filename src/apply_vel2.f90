3cf81f73a87ae12f58629668588b845a6ad79bd9
Xiaoming Wang <40604687+Xiaoming-Wang1@users.noreply.github.com>
2021-01-12 14:22:12 -0500

    Update apply_vel2.f90

--- src/apply_vel2.f90
+++ src/apply_vel2.f90
@@ -1,6 +1,6 @@
 ! first and second derivative of Hamiltonian
 !-----------------------------------------------------------------------
-SUBROUTINE apply_vel(psi, vel_psi, ik, ipol)
+SUBROUTINE apply_vel2(psi, vel_psi, vel2_psi, ik)
   !-----------------------------------------------------------------------
   !
   ! ... Apply the velocity operator
@@ -12,44 +12,99 @@ SUBROUTINE apply_vel(psi, vel_psi, ik, ipol)
   USE kinds,                ONLY : DP
   USE ions_base,            ONLY : nat, ityp, ntyp => nsp
   USE klist,                ONLY : xk
-  USE wvfct,                ONLY : nbnd, npwx, npw, igk, current_k
-  USE lsda_mod,             ONLY : current_spin, isk
+  USE wvfct,                ONLY : nbnd, npwx, npw, igk, current_k, et
+  USE lsda_mod,             ONLY : current_spin, isk, lsda
   USE becmod,               ONLY : becp
   USE cell_base,            ONLY : tpiba
-  USE g_tensor_module,      ONLY : init_us_2_no_phase
-  USE g_tensor_module,      ONLY : q_gipaw
+  USE orbm_module,          ONLY : q_orbm
+  USE noncollin_module,     ONLY : noncolin, npol
   USE pwcom
 
   !-- paramters ----------------------------------------------------------
   IMPLICIT NONE
   INTEGER, INTENT(IN) :: ipol       ! cartesian direction (1..3)
   INTEGER, INTENT(IN) :: ik         ! k-point
-  COMPLEX(DP), INTENT(IN) :: psi(npwx,nbnd)
-  COMPLEX(DP), INTENT(OUT) :: vel_psi(npwx,nbnd)
+  COMPLEX(DP), INTENT(IN) :: psi(npwx*npol,nbnd)
+  COMPLEX(DP), INTENT(OUT) :: vel_psi(npwx*npol,nbnd,3)
+  COMPLEX(DP), INTENT(OUT) :: vel2_psi(npwx*npol,nbnd,3,3)
 
   !-- local variables ----------------------------------------------------
   real(dp), parameter :: ryd_to_hartree = 0.5d0
   !real(dp), parameter :: q_gipaw = 0.02d0
-  complex(dp), allocatable :: aux(:,:), vkb_save(:,:)
+  complex(dp), allocatable :: aux(:,:), vkb_save(:,:), vq0(:)
+  complex(dp), allocatable :: vqp(:,:,:), vqm(:,:,:)
+  complex(dp), allocatable :: vq2p(:,:,:), vq2m(:,:,:)
   real(dp) :: dk, oldk(3)
-  integer :: isign
+  integer :: isign, iqp(3,3), iqm(3,3), iq2p(3,3), iq2m(3,3),i
 
-  call start_clock('apply_vel')
+  call start_clock('apply_vel2')
+  
   vel_psi = (0.d0,0.d0)
+  vel2_psi = (0.d0,0.d0)
+  
+  iq(:,1) = (/1,0,0/)
+  iq(:,2) = (/0,1,0/)
+  iq(:,3) = (/0,0,1/)
+
+  iq2(:,1) = (/0,1,1/)
+  iq2(:,2) = (/1,0,1/)
+  iq2(:,3) = (/1,1,0/)
+
+  
+  allocate(vq(npwx*npol,nband,3,2))
+  allocate(vq2p(npwx*npol,nband,3,2))
+  allocate(vq0(npwx*npol))
+  
+  do ibnd = 1, nbnd
+     vq0(:) = psi(:,ibnd)*et(ibnd,ik)*ryd_to_hartree
+  enddo
 
   ! set dk (= delta_k ?)
   !dk = q_gipaw/2.d0/tpiba
-  dk = q_gipaw/tpiba
+  dk = q_orbm/2.d0
   
   ! allocate temporary arrays, save old NL-potential
-  allocate(aux(npwx,nbnd), vkb_save(npwx,nkb))
+  allocate(vkb_save(npwx,nkb))
+  call allocate_bec_type(nkb, nbnd, becp)
+  
   vkb_save = vkb
   oldk(:) = xk(:,ik)
 
   current_k = ik
   if (lsda) current_spin = isk(ik)
-  call gk_sort(xk(1,ik), ngm, g, ecutwfc/tpiba2, npw, igk, g2kin)
-
+  npw = ngk(ik)
+  
+  
+  do i = 1,3
+     xk(:,ik) = oldk(:,ik) + iq(:,1)*dk
+     call g2_kin(ik)
+     call init_us_2_no_phase(npw, igk_k(1,ik), xk(1,ik), vkb)
+     call h_psi(npwx, npw, nbnd, psi, vq(:,:,i,1))
+  enddo
+  do i = 1,3
+     xk(:,ik) = oldk(:,ik) - iq(:,1)*dk
+     call g2_kin(ik)
+     call init_us_2_no_phase(npw, igk_k(1,ik), xk(1,ik), vkb)
+     call h_psi(npwx, npw, nbnd, psi, vq(:,:,i,2))
+  enddo
+  do i = 1,3
+     xk(:,ik) = oldk(:,ik) + iq2(:,1)*dk
+     call g2_kin(ik)
+     call init_us_2_no_phase(npw, igk_k(1,ik), xk(1,ik), vkb)
+     call h_psi(npwx, npw, nbnd, psi, vq2(:,:,i,1))
+  enddo
+  do i = 1,3
+     xk(:,ik) = oldk(:,ik) - iq2(:,1)*dk
+     call g2_kin(ik)
+     call init_us_2_no_phase(npw, igk_k(1,ik), xk(1,ik), vkb)
+     call h_psi(npwx, npw, nbnd, psi, vq2(:,:,i,2))
+  enddo
+  
+  call deallocate_bec_type (becp)
+  
+  do i = 1,3
+     vel_psi(:,:,i) = (vq(:,:,i,1) + vq(:,:,i,1))*ryd_to_hartree/(2.d0*dk*tpiba)
+  enddo
   !====================================================================
   ! compute (1/2|dk|) ( H_{k+dk} |psi> - H_{k-dk}|psi> )
   !====================================================================
@@ -75,4 +130,4 @@ SUBROUTINE apply_vel(psi, vel_psi, ik, ipol)
 
   call stop_clock('apply_vel')
 
-END SUBROUTINE apply_vel
+END SUBROUTINE apply_vel2
