ed5dc1a1d72dfa2f675aa7437003b83b33e50a32
Xiaoming Wang <40604687+Xiaoming-Wang1@users.noreply.github.com>
2021-01-12 17:11:34 -0500

    Update apply_vel2.f90

--- src/apply_vel2.f90
+++ src/apply_vel2.f90
@@ -10,19 +10,17 @@ SUBROUTINE apply_vel2(psi, vel_psi, vel2_psi, ik)
   ! ...   V^{NL} => V^{NL} * ryd_to_hartree
   !-----------------------------------------------------------------------
   USE kinds,                ONLY : DP
-  USE ions_base,            ONLY : nat, ityp, ntyp => nsp
-  USE klist,                ONLY : xk
-  USE wvfct,                ONLY : nbnd, npwx, npw, igk, current_k, et
+  USE klist,                ONLY : xk, igk_k, ngk
+  USE wvfct,                ONLY : nbnd, npwx, npw, current_k, et
   USE lsda_mod,             ONLY : current_spin, isk, lsda
-  USE becmod,               ONLY : becp
+  USE becmod,               ONLY : becp, allocate_bec_type, deallocate_bec_type
   USE cell_base,            ONLY : tpiba
+  USE uspp,                 ONLY : nkb, vkb
   USE orbm_module,          ONLY : q_orbm
-  USE noncollin_module,     ONLY : noncolin, npol
-  USE pwcom
+  USE noncollin_module,     ONLY : npol
 
   !-- paramters ----------------------------------------------------------
   IMPLICIT NONE
-  INTEGER, INTENT(IN) :: ipol       ! cartesian direction (1..3)
   INTEGER, INTENT(IN) :: ik         ! k-point
   COMPLEX(DP), INTENT(IN) :: psi(npwx*npol,nbnd)
   COMPLEX(DP), INTENT(OUT) :: vel_psi(npwx*npol,nbnd,3)
@@ -30,12 +28,11 @@ SUBROUTINE apply_vel2(psi, vel_psi, vel2_psi, ik)
 
   !-- local variables ----------------------------------------------------
   real(dp), parameter :: ryd_to_hartree = 0.5d0
-  !real(dp), parameter :: q_gipaw = 0.02d0
-  complex(dp), allocatable :: aux(:,:), vkb_save(:,:), vq0(:)
-  complex(dp), allocatable :: vqp(:,:,:), vqm(:,:,:)
-  complex(dp), allocatable :: vq2p(:,:,:), vq2m(:,:,:)
+  complex(dp), allocatable :: vkb_save(:,:), vq0(:,:)
+  complex(dp), allocatable :: vq(:,:,:,:)
+  complex(dp), allocatable :: vq2(:,:,:,:)
   real(dp) :: dk, oldk(3)
-  integer :: isign, iqp(3,3), iqm(3,3), iq2p(3,3), iq2m(3,3),i
+  integer :: iq(3,3), iq2(3,3), i, ibnd
 
   call start_clock('apply_vel2')
   
@@ -51,16 +48,15 @@ SUBROUTINE apply_vel2(psi, vel_psi, vel2_psi, ik)
   iq2(:,3) = (/1,1,0/)
 
   
-  allocate(vq(npwx*npol,nband,3,2))
-  allocate(vq2p(npwx*npol,nband,3,2))
-  allocate(vq0(npwx*npol))
+  allocate(vq(npwx*npol,nbnd,3,2))
+  allocate(vq2(npwx*npol,nbnd,3,2))
+  allocate(vq0(npwx*npol,nbnd))
   
   do ibnd = 1, nbnd
-     vq0(:,ibnd) = psi(:,ibnd)*et(ibnd,ik)*ryd_to_hartree
+     vq0(:,ibnd) = psi(:,ibnd)*et(ibnd,ik)
   enddo
 
-  ! set dk (= delta_k ?)
-  !dk = q_gipaw/2.d0/tpiba
+
   dk = q_orbm/2.d0
   
   ! allocate temporary arrays, save old NL-potential
@@ -78,25 +74,25 @@ SUBROUTINE apply_vel2(psi, vel_psi, vel2_psi, ik)
   do i = 1,3
      xk(:,ik) = oldk(:,ik) + iq(:,1)*dk
      call g2_kin(ik)
-     call init_us_2_no_phase(npw, igk_k(1,ik), xk(1,ik), vkb)
+     call init_us_2(npw, igk_k(1,ik), xk(1,ik), vkb)
      call h_psi(npwx, npw, nbnd, psi, vq(:,:,i,1))
   enddo
   do i = 1,3
      xk(:,ik) = oldk(:,ik) - iq(:,1)*dk
      call g2_kin(ik)
-     call init_us_2_no_phase(npw, igk_k(1,ik), xk(1,ik), vkb)
+     call init_us_2(npw, igk_k(1,ik), xk(1,ik), vkb)
      call h_psi(npwx, npw, nbnd, psi, vq(:,:,i,2))
   enddo
   do i = 1,3
      xk(:,ik) = oldk(:,ik) + iq2(:,1)*dk
      call g2_kin(ik)
-     call init_us_2_no_phase(npw, igk_k(1,ik), xk(1,ik), vkb)
+     call init_us_2(npw, igk_k(1,ik), xk(1,ik), vkb)
      call h_psi(npwx, npw, nbnd, psi, vq2(:,:,i,1))
   enddo
   do i = 1,3
      xk(:,ik) = oldk(:,ik) - iq2(:,1)*dk
      call g2_kin(ik)
-     call init_us_2_no_phase(npw, igk_k(1,ik), xk(1,ik), vkb)
+     call init_us_2(npw, igk_k(1,ik), xk(1,ik), vkb)
      call h_psi(npwx, npw, nbnd, psi, vq2(:,:,i,2))
   enddo
   
@@ -119,29 +115,14 @@ SUBROUTINE apply_vel2(psi, vel_psi, vel2_psi, ik)
   vel2_psi(:,:,1,3) = vel2_psi(:,:,3,1)
   vel2_psi(:,:,2,1) = vel2_psi(:,:,1,2)
 
-  !====================================================================
-  ! compute (1/2|dk|) ( H_{k+dk} |psi> - H_{k-dk}|psi> )
-  !====================================================================
-  allocate(becp(nkb,nbnd))
-  do isign = -1,1,2
-    xk(ipol,ik) = oldk(ipol) + isign * dk     ! k + dk
-
-    ! compute <\beta(k \pm dk)| and project on |psi>
-    call g2_kin(ik)
-    call init_us_2_no_phase(npw, igk, xk(1,ik), vkb)
-    aux = (0.d0,0.d0)
-    call h_psi(npwx, npw, nbnd, psi, aux)
-    vel_psi = vel_psi + dble(isign) * ryd_to_hartree * aux/(2.d0*dk*tpiba)
-  enddo
-  deallocate(becp)
 
   ! restore NL-potential at k
   xk(:,ik) = oldk(:)
   vkb = vkb_save
   
   ! free memory
-  deallocate(aux, vkb_save)
+  deallocate(vkb_save, vq0, vq, vq2)
 
-  call stop_clock('apply_vel')
+  call stop_clock('apply_vel2')
 
 END SUBROUTINE apply_vel2
